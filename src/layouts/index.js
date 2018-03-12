fe28b065f6f91c660f98e0fedd92755a19d4468c
Eric Slusarz <ericslusarz@gmail.com>
2018-03-12 09:51:02 -0400

    Second Commit

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,29 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
-import Header from '../components/Header'
-import './index.css'
+import Navbar from '../components/Navbar'
+import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet
-      title="Gatsby Default Starter"
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
-    <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
+    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Navbar />
+    <div>{children()}</div>
   </div>
 )
 
